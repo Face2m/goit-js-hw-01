0d3e115daf2de4b08d42667a29b0c126b9342574
face2m <tsekhmistro.dima@gmail.com>
2026-01-28 18:13:38 +0200

    js-hw

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     if (customerCredits < totalPrice) {
         return `Insufficient funds!`
     }
-    else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
