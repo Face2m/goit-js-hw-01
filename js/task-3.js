4eaebf40e2e4de3097aedff6a36526689b41c016
face2m <tsekhmistro.dima@gmail.com>
2026-01-28 15:32:03 +0200

    my 2nd js-hw

--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,21 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+
+
+function checkAge(age) {
+  if (age === 20 || age === 30) {
+    `You are an adult`
+  }
+  if (age === 10 || age === 17) {
+    `undefined`
+  }
+  return age
+}
+
+checkAge(20)
+checkAge(17)
+checkAge(10)
+checkAge(30)
